7554e8026d8bbf211bdc2da3cd15c785b69e5380
tdfdlead <srinivasa@think.design>
2017-09-08 18:29:13 +0530

    added all files

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 "use strict";
 
 // primitive data types: Numbers,String,Null, Undefined, Boolean, Symbol
+
+var a= [1,2,3];
+
+var b =[[1,2,3],[4,5,6]];
+
+var c = [[1, 2, 3], [4, 5, 6]];
+
+// Swithc Statement
+
+var a=25;
+var result= a%2;
+switch (result){
+
+	case 0:
+	alert("Even Number");
+	break;
+	default:
+	alert ("Odd Number");
+	break;
+
+}
+
+var z=5;
+for( var x=1;x<=10; x++){
+		document.write(z*x);
+}
